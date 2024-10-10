551f0baa25253d72c36598861ca42655970beacf
alvis051 <chenhangoh051@gmail.com>
2024-10-10 14:30:01 +0800

    Arrays with Generics

---
+++ arrays/collection_fun.go
@@ -0,0 +1,18 @@
+package main
+
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
+	}
+	return result
+}
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
--- arrays/sum.go
+++ arrays/sum.go
@@ -25,11 +25,3 @@ func SumAllTails(numbers ...[]int) []int {
 	}
 	return Reduce(numbers, sumTail, []int{})
 }
-
-func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
-	var result = initialValue
-	for _, x := range collection {
-		result = f(result, x)
-	}
-	return result
-}
